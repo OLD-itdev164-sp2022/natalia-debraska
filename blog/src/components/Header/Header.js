0f02f7b5f6d5dba42495d99a420a97a4c60b71eb
ndebraska <castron1@gmatc.matc.edu>
2022-04-28 09:42:47 -0500

    Heading1

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -1,7 +1,8 @@
-import * as React from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from 'styled-components'
+import { H1 } from '../Heading'
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -12,10 +13,6 @@ const Inner = styled.div`
   margin: 0px auto;
   max-width: 960px;
   padding: 1.45rem 1.0875rem;
-`
-
-const H1 = styled.h1`
-  margin: 0px;
 `
 
 const StyledLink = styled(Link)`
