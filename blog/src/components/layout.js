ca8f58516862de64d938b49734fac746bd0d4f84
ndebraska <castron1@gmatc.matc.edu>
2022-04-28 12:08:32 -0500

    Layout

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -13,6 +13,8 @@ import styled, { ThemeProvider } from 'styled-components'
 import { Gray } from "../themes/Gray"
 import { Header } from "./Header"
 import GlobalStyle from "./GlobalStyle"
+import { Main } from './Main'
+import { Footer } from './Footer'
 
 const Content = styled.div`
   margin: 0 auto;
@@ -35,18 +37,14 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={Gray}>
       <GlobalStyle />
-      <Header siteTitle={data.site.siteMetadata.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata.title} />
       <Content>
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
+        <Main>{children}</Main>
+        <Footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </Footer>
       </Content>
     </ThemeProvider>
   )
